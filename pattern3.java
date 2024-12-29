c8833a9778d8f7f086d75cfcad27b60c63c4a0df
Qasim Kharodia <qasim.k@somaiya.edu>
2024-12-29 19:42:05 +0530

    Solved third pattern problem

--- pattern3.java
+++ pattern3.java
@@ -1,17 +1,20 @@
 import java.util.Scanner;
 
-public class pattern2 {
+public class pattern3 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of rows: ");
-        int n = scanner.nextInt();
+        int n = sc.nextInt();
 
-        for (int i = n; i >= 1; i--) {
+        for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= n - i; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
-        scanner.close();
+        sc.close();
     }
 }
