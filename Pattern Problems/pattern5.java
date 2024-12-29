238672aa67b421cd79c5e1827ff0a27d82a9a678
Qasim21569 <148331508+Qasim21569@users.noreply.github.com>
2024-12-29 22:23:02 +0530

    Inverted Full Pyramid Pattern problem

--- Pattern Problems/pattern5.java
+++ Pattern Problems/pattern5.java
@@ -7,7 +7,7 @@ public class pattern5 {
         int n = sc.nextInt();
 
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i; j++) {
+            for (int j = 1; j <= i; j++) { 
                 System.out.print(" ");
             }
             for (int j = 1; j <= (2 * (n - i) + 1); j++) {
